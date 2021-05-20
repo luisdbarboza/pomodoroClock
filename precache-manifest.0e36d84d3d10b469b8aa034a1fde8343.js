da0309c07d9e8405858de37b1fe3b562c60050ba
Luis Barboza <lbarbozanav@gmail.com>
2021-05-20 06:36:43 -0400

    Updates

---
+++ precache-manifest.0e36d84d3d10b469b8aa034a1fde8343.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49fbff22028f8cf887fc581dbca96e78",
+    "url": "/pomodoroClock/index.html"
+  },
+  {
+    "revision": "ba62bb2fe18a9f9483fb",
+    "url": "/pomodoroClock/static/css/main.7a3fb395.chunk.css"
+  },
+  {
+    "revision": "39d7e7c58b741076e257",
+    "url": "/pomodoroClock/static/js/2.b3e5beb0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pomodoroClock/static/js/2.b3e5beb0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba62bb2fe18a9f9483fb",
+    "url": "/pomodoroClock/static/js/main.2181f848.chunk.js"
+  },
+  {
+    "revision": "437b8c34c89c86a95470",
+    "url": "/pomodoroClock/static/js/runtime-main.4518de97.js"
+  }
+]);
